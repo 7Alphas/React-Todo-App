24ec6284c625ab3b3bf354d5cc9ab4c581cf46a6
Egenti Alpha <egenti54@gmail.com>
2025-04-14 21:17:23 +0100

    Add Task Form

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Task from './Task';
+import TaskForm from './TaskForm';
+
+function App() {
+  return (
+   <div>
+    <TaskForm />
+    <Task />
+   </div>
+  );
+}
+
+export default App;
