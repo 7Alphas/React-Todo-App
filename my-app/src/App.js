3be81a96faba990c9ca7b41fcc92c4240f608d1f
Egenti Alpha <egenti54@gmail.com>
2025-04-16 18:49:48 +0100

    Local Storage Update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,16 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Task from './Task';
 import TaskForm from './TaskForm';
 
 function App() {
+  const [tasks,setTasks] = useState([]);
+  useEffect( () => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+  useEffect(() => {}, []);
+  function addTask(name) {
+setTasks(prev => {
+  return [...prev, {name:name,done:false}];
+});
+  }
   return (
    <main>
-    <TaskForm />
-    <Task />
-    <Task />
-    <Task />
-    <Task />
-    <Task />
+    <TaskForm onAdd={addTask} />
+    {tasks.map(task => (
+      <Task {...task}/>
+    ))}
    </main>
   );
 }
--- my-app/src/TaskForm.js
+++ my-app/src/TaskForm.js
@@ -1,10 +1,15 @@
 import React, { useState } from 'react'
 
 
-export default function TaskForm() {
+export default function TaskForm({onAdd}) {
     const [taskName, setTaskName] = useState('');
+    function handleSubmit(ev) {
+ev.preventDefault();
+onAdd(taskName);
+setTaskName('');
+    }
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <button>+</button>
             <input type="text"
                 value={taskName}
